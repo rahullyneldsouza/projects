fe52218f17f004ffbf6ef53bfdd721b6d4a7566a
Rahul DSouza <rdsouza@asavie.com>
2025-02-21 15:21:08 +0530

    K8s version update

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@
 resource "linode_lke_cluster" "k8s" {
   label = "jenkins-k8s"
   region = "us-east"
-  k8s_version = "1.22"
+  k8s_version = "1.32"
   pool {
     type  = "g6-standard-1"
     count = 2
